c413edc5667e7c0cbed1a6e63f862d5df46ea6e3
gvbrielflores <gabrielf.lovo@gmail.com>
2025-03-27 17:26:02 -0500

    pepper works, login works. changed insert query creator to include pepper

--- scripts/user_registration_command.py
+++ scripts/user_registration_command.py
@@ -3,25 +3,33 @@ import psycopg2
 import argparse
 import random
 import hashlib
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 def main():
     try:
         randint = random.randint(0, 1898989)
         address = f"bsddress{str(randint)}"
         username = f"username{str(randint)}"
+        role="A"
         parser = argparse.ArgumentParser(prog="user_registration_command", description="generate the sql command to add a new user to db."
         " also prints out salt and passhash for general purpose use")
         parser.add_argument('password', help='the password you want to salt and hash')
         parser.add_argument('-u', help='optional username')
         parser.add_argument('-a', help='optional address')
+        parser.add_argument('-r', help='optional role')
         args = parser.parse_args()
         passwd = args.password
         username = args.u
         address = args.a
+        role = args.r
         salt = bcrypt.gensalt()
-        passhash = bcrypt.hashpw(passwd.encode('utf-8'), salt)
+        pepper = os.getenv('PEPPER')
+        passhash = bcrypt.hashpw(passwd.encode('utf-8'), salt+pepper.encode('utf-8'))
         
-        print(f"INSERT INTO accounts VALUES ('{address}', '{username}', '{salt.hex()}', '{passhash.hex()}', 'A');")
+        print(f"INSERT INTO accounts VALUES ('{address}', '{username}', '{salt.hex()}', '{passhash.hex()}', '{role}');")
 
         return 0
 
