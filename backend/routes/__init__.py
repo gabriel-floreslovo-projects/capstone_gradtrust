c900baa0bee2dae7f0ef439ffd35cbd7d1415118
Segun Oluwole <segunoluwole30@gmail.com>
2025-04-01 17:12:20 -0500

    delete files from frontend folder

--- backend/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-api = Blueprint('api', __name__, url_prefix='/api')
-
-# Register api routes
-from .credentials import credentials
-
-api.register_blueprint(credentials)
