449e36fd21cdc70a812f8957345780317274ccc2
Blake Dejohn <unknowingly@tamu.edu>
2025-04-10 17:08:46 -0500

    added evenlet so that it can be used for websockets and HTTP

--- backend/socketio_instance.py
+++ backend/socketio_instance.py
@@ -1,4 +1,4 @@
 from flask_socketio import SocketIO
 
 # Initialize SocketIO
-socketio = SocketIO(cors_allowed_origins="*", async_mode="threading")
+socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
