6712aa25b0396954e3b7b17f28c861d3ba9e2ea3
gvbrielflores <gabrielf.lovo@gmail.com>
2025-04-14 13:20:51 -0500

    debugging role based locking

--- frontend/src/my-app/app/middleware.ts
+++ frontend/src/my-app/app/middleware.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtDecrypt, jwtVerify } from 'jose';
 import next from "next";
+import { access } from "fs";
 
 const jwtSecret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
 
 export async function middleware(req:NextRequest) {
     const accessToken = req.cookies.get('access_token')?.value;
+    console.log(`secret: ${jwtSecret}`);
+    console.log("access token: ", accessToken);
 
     if (!accessToken) { // If the request has no access token, the user must sign in to get one
         return NextResponse.redirect(new URL('/Sign_In', req.url));
@@ -16,6 +19,8 @@ export async function middleware(req:NextRequest) {
 
         const role = payload.role;
         const pathname = req.nextUrl.pathname;
+
+        console.log(`JWT role: ${role}. pathname: ${pathname}`);
 
         // Role based locking
         if (pathname.startsWith('/admin') && role != "admin") {
