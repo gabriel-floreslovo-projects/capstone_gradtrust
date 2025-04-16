0b484f454ece466e49e776f5bbb75eef1a5fa55b
Blake Dejohn <unknowingly@tamu.edu>
2025-04-16 18:00:40 -0500

    changing file directory structure (does create issuer page need to be in its own folder?)

--- frontend/src/my-app/app/admin/create-issuer/page.tsx
+++ frontend/src/my-app/app/admin/create-issuer/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import Navbar from "../../components/navbar";
-import Footer from "../../components/footer";
+import Navbar from "../../../components/navbar";
+import Footer from "../../../components/footer";
 
 export default function CreateIssuerPage() {
   const [issuerName, setIssuerName] = useState("");
