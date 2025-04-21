08f58c4afea33813535f99f10605b63370b3b1bd
segunoluwole30 <segunoluwole30@gmail.com>
2025-04-20 21:05:13 -0500

    dont need to overwrite entropy value because institution is assigned one entropy value upon first registration

--- backend/routes/issuer.py
+++ backend/routes/issuer.py
@@ -36,7 +36,6 @@ def register_issuer():
                     DO UPDATE SET 
                         name = EXCLUDED.name,
                         signature = EXCLUDED.signature
-                        entropy = EXCLUDED.entropy
                     """,
                     (issuer_address, issuer_name, signature, entropy)
                 )
